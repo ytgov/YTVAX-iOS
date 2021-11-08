3bf36a7631ab011b109b753e9d18dc5ce4691ab8
Mohamed Afsar <afsar@freshworks.io>
2021-11-08 19:45:35 +0530

    feat: Setup project

---
+++ YukonVaccinationVerifier/YukonVaccinationVerifier/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  YukonVaccinationVerifier
+//
+//  Created by Mohamed Afsar on 01/11/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
