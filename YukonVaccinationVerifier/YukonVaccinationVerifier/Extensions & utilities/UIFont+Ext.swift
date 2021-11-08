314c8d7b4715f8a422b7d1646e0bb6fdd65522f0
Mohamed Afsar <afsar@freshworks.io>
2021-11-08 19:55:15 +0530

    feat: Create MVP

---
+++ YukonVaccinationVerifier/YukonVaccinationVerifier/Extensions & utilities/UIFont+Ext.swift
@@ -0,0 +1,23 @@
+//
+//  UIFont+Ext.swift
+//  YukonVaccinationVerifier
+//
+//  Created by Mohamed Afsar on 02/11/21.
+//
+
+import UIKit
+
+internal extension UIFont {
+    enum ThemeStyle: String {
+        case regular = "Montserrat-Regular"
+        case medium = "Montserrat-Medium"
+        case italic = "Montserrat-Italic"
+        case boldItalic = "Montserrat-BoldItalic"
+        case bold = "Montserrat-Bold"
+        
+    }
+    
+    static func themeFont(size: CGFloat, style: ThemeStyle = .regular) -> UIFont {
+        UIFont(name: style.rawValue, size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+}
